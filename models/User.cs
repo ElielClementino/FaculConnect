c5634c0bf92dd32e938cfb348f5d6cfbf8c1d899
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-01 21:34:43 -0300

    Add password field and Contraints to user

--- models/User.cs
+++ models/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace models;
 
 public class User {
@@ -5,9 +7,20 @@ public class User {
     public User() {
         CreatedAT = DateTime.UtcNow;
     }
-
+    [Key]
     public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string Username { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Password { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
     public string Email { get; set; }
     public DateTime CreatedAT { get; set; }
 }
