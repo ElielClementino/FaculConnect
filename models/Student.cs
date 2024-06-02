9e3d0677bb645848712dba922b444269e8ec3b9a
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-02 15:14:40 -0300

    Add Course fk on Student Model

--- models/Student.cs
+++ models/Student.cs
@@ -43,4 +43,8 @@ public class Student {
     public int? UserId { get; set; }
 
     public User? User { get; set; }
+
+    public int? CourseId { get; set; }
+
+    public Course? Course { get; set; }
 }
