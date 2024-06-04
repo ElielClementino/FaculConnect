32a92f30bfdae88604093cd68a310fad2f17689c
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-03 23:38:50 -0300

    Add register request

--- Frontend/api/user.js
+++ Frontend/api/user.js
@@ -5,10 +5,20 @@ export default {
         return new Promise((resolve, reject) => {
             api.post("login", userInfo)
             .then((response) => {
-                return resolve(response.data)
+                return resolve(response.data);
             }).catch((error) => {
-                return reject(error)
+                return reject(error);
             })
         })
     },
+    register: (userInfo) => {
+        return new Promise((resolve, reject) => {
+            api.post("register", userInfo)
+            .then((response) => {
+                return resolve(response.data);
+            }). catch((error) => {
+                return reject(error);
+            })
+        })
+    }
 }
