e058efff2561a406554a10a5c90ea3bf8324a1da
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-04 17:08:03 -0300

    Add Navbar component

---
+++ Frontend/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app-bar
+        color="#FFFFFF"
+        fixed
+        class="nav-items"
+    >
+        <v-app-bar-nav-icon color="#000000">
+            <v-icon x-large>mdi-bell-outline</v-icon>
+        </v-app-bar-nav-icon>
+        <v-app-bar-nav-icon color="#000000">
+            <v-icon x-large>mdi-account-circle-outline</v-icon>
+        </v-app-bar-nav-icon>
+
+    </v-app-bar>
+</template>
+
+<style scoped>
+.nav-items {
+    display:flex;
+    align-items: end;
+    justify-content: flex-end;
+}
+
+</style>
