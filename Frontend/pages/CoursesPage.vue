88fd9e4d2b9714ed69e9c7a114a8387d1f4dada0
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-05 22:56:18 -0300

    Retrive data from backend and assign to corresponding fields

--- Frontend/pages/CoursesPage.vue
+++ Frontend/pages/CoursesPage.vue
@@ -1,38 +1,93 @@
 <template>
-<v-app style="background-color:#ffffff;">
-  <SideBar />
-  <NavBar />
-  <v-card
-    style="box-shadow: none;"
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-card-text class="bg-black card">
-      <div style="text-align:center;" class="text-style color-white">
-      <h1 class="course-title">ANÁLISE E DESENVOLVIMENTO DE SISTEMAS</h1>
-      </div>
-      <v-divider style="border-color:#FFFFFF; margin-top:8px"></v-divider>
-      <div class="course-info">
-      <p style="font-size:2vh;" class="text-style color-green">INSCRIÇÕES ABERTAS</p>
-      <p style="font-size:2vh;" class="text-style color-light-green">R$ 120,00/Mês</p>
-      </div>
-      <div class=" text-style color-white">
-        <p>Este curso ensina a projetar, implementar e manter softwares, abordando desde a análise de requisitos até a programação e testes. Prepara profissionais para desenvolver soluções tecnológicas e eficazs e inovadoras.</p>
-      </div>
-    </v-card-text>
-    <div class="div-button">
-      <v-btn
-        style="padding:25px;"
-        color=#0856CA
+  <v-app style="background-color:#ffffff;">
+    <SideBar />
+    <NavBar />
+    <v-container
+      class="courses-grid"
+    >
+      <v-card
+        class="course-card"
+        v-for="course in courses" :key="course.id"
       >
-        <p style="margin-bottom:0" class="text-style color-white">Se inscrever</p>
-      </v-btn>
-    </div>
-  </v-card>
-</v-app>
+        <v-card-text class="bg-black card-content" style="border-radius:25px;">
+          <div style="text-align:center;" class="text-style color-white">
+            <h1 class="course-title"> {{ course.name }}</h1>
+          </div>
+          <v-divider style="border-color:#FFFFFF; margin-top:24px"></v-divider>
+          <div class="course-info">
+            <p style="font-size:2vh;" :class="course.availability ? 'color-green' : 'color-red'" class="text-style">{{ course.availability ? "INSCRIÇÕES ABERTAS" : "INSCRIÇÕES FECHADAS"}}</p>
+            <p style="font-size:2vh;" class="text-style color-light-green">R$ {{ course.price }},00/Mês</p>
+          </div>
+          <div class="text-style color-white">
+            <p>{{ course.description }}</p>
+          </div>
+        </v-card-text>
+        <div class="div-button">
+          <v-btn
+            style="padding:25px;"
+            color="#0856CA"
+            :class="course.availability ? '': 'disabled-btn'"
+          >
+            <p style="margin-bottom:0" class="text-style color-white">Se inscrever</p>
+          </v-btn>
+        </div>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
+
+
+<script>
+
+import course from "../api/course.js"
+
+export default {
+  data() {
+    return {
+      courses: {}
+    }
+  },
+  async mounted() {
+    await this.listCourses()
+  },
+  methods: {
+    async listCourses() {
+      try {
+        this.courses = await course.listCourses()
+        console.log(this.courses)
+      } catch(error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
+
 <style>
+.courses-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+  margin-top: 100px;
+  margin-right: 7%;
+  margin-bottom: 100px;
+}
+
+.course-card {
+  display: flex;
+  flex-direction: column;
+  text-align:center;
+  justify-content: space-between;
+  width: 80%;
+  height: 100%;
+  background-color: #ffffff !important;
+  box-shadow:none !important;
+}
+
+.card-content {
+  flex-grow: 1;
+}
 
 .color-white {
     color: #FFFFFF;
@@ -55,31 +110,31 @@
 }
 
 .course-info {
-    display:flex;
-    justify-content:space-between;
-    margin-bottom: 14px;
+    display: flex;
+    justify-content: space-between;
+    margin-top:14px;
 }
 
 .course-title {
-    font-size:5vh;
+    font-size: 5vh;
     line-height: 6vh;
 }
 
-.card {
-    border-radius: 12px;
-    box-shadow: 0 0 2px rgba(0, 0, 0, 0.1);
-}
-
 .div-button {
-    display:flex;
-    justify-content:center;
-    text-align:center;
-    background-color: #ffffff;
+    text-align: center;
+    background-color: rgb(255, 255, 255, 1);
+    border: none;
+
 }
 
 .text-style {
     font-family: Poppins, sans-serif;
     font-weight: 900;
+}
 
+.disabled-btn {
+  background-color: #0856CA !important;
+  opacity: 0.5;
+  cursor: not-allowed;
 }
 </style>
