9a2966571fcd97ba8cc780bfb6d4ad5ca039671e
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2024-06-06 16:27:06 -0300

    Add props to nav for dynamic title

--- Frontend/components/NavBar.vue
+++ Frontend/components/NavBar.vue
@@ -2,8 +2,9 @@
     <v-app-bar
         color="#FFFFFF"
         fixed
-        class="nav-items"
+        class="nav-items text-style "
     >
+        <h1 text style="color:#010101;" class="nav-title">{{ NavTitle }}</h1>
         <v-app-bar-nav-icon color="#000000">
             <v-icon x-large>mdi-bell-outline</v-icon>
         </v-app-bar-nav-icon>
@@ -13,12 +14,35 @@
 
     </v-app-bar>
 </template>
+<script>
+export default {
+    props: {
+        NavTitle: {
+            type:String,
+            required: false,
+            default: ''
+        }
+    },
+    computed: {
+        showTitle() {
+            return this.NavTitle !== '';
+        }
+  }
+}
+</script>
 
 <style scoped>
 .nav-items {
     display:flex;
     align-items: end;
     justify-content: flex-end;
+    padding-right: 10px;
 }
-
+.nav-title {
+    padding: 100px;
+}
+.text-style {
+    font-family: Poppins, sans-serif;
+    font-weight: 900;
+}
 </style>
